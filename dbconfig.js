91193b1ba373269009103d06c9eee01b29060bbd
Alredho <alredho03@gmail.com>
2023-09-11 10:37:56 +0700

    Added connection to local mysql using sequelize

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('money_management', 'root', '0000', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql'
+});
+
+sequelize.authenticate().then(() => {
+    console.log("Connected to database!");
+}).catch(() => {
+    console.log("Could not connect!");
+});
+
